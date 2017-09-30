b76ce26d834e48139ba191511a6f1dcb3fced1f1
Manvendra Singh <Buffer0x7cd@users.noreply.github.com>
2017-10-01 02:29:44 +0530

    Update settings.py

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^$2ib2a%a2#sy*zx)2sxk7)qgb9&j$o5hxk3cj001nsekq0vqi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['buffer0x7cd.pythonanywhere.com']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
