692fa9ba236aa63b626cb6ee1075ce08eeadf7e7
buffer0x7cd <manvendra021997@gmail.com>
2017-10-02 18:41:51 +0530

    Made some funny changes

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^$2ib2a%a2#sy*zx)2sxk7)qgb9&j$o5hxk3cj001nsekq0vqi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
