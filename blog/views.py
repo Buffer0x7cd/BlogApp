704d5fe9ededce4f78d3938b4c866d7ed50538a1
buffer0x7cd <manvendra021997@gmail.com>
2017-10-03 19:44:04 +0530

    Added save as a draft option

--- blog/views.py
+++ blog/views.py
@@ -5,43 +5,53 @@ from .forms import PostForm, FeedbackForm
 from django.utils import timezone, http
 from django.http import HttpResponse
 # Create your views here.
+
 def post_list(request):
+    ''' Render the list of published post'''
     posts =  Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     context = {'posts':posts}
     return render(request, 'blog/post_list.html', context )
 
+
 @login_required
 def post_new(request):
+    ''' Create a new post'''
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post  = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post.pk)
     else:
         form =  PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
+@login_required
 def post_detail(request, pk):
+    ''' Show full blog'''
     post =  get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
 
+
 @login_required
 def post_edit(request, pk):
+    ''' Edit a post'''
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
 def sendfeedback(request):
@@ -57,3 +67,23 @@ def sendfeedback(request):
     else:
         form = FeedbackForm()
         return render(request, 'blog/feedback.html', {'form':form})
+
+
+@login_required
+def drafts(request):
+    ''' Render the list of unpublished posts'''
+    posts =  Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/drafts.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post =  get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def delete(request, pk):
+    ''' Delete a post'''
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
